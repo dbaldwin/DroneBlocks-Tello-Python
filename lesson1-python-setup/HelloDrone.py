4c3845cf7d8ae39febdc182b0a14c7c37164d082
Dennis Baldwin <dennisbaldwin@gmail.com>
2018-04-06 14:21:23 -0500

    HelloDrone edits

--- lesson1-python-setup/HelloDrone.py
+++ lesson1-python-setup/HelloDrone.py
@@ -1,11 +1,9 @@
+# This script is part of our course on Tello drone programming
+# https://learn.droneblocks.io/p/tello-drone-programming-with-python/
 
-# coding: utf-8
-
-# In[17]:
-
-
+# Say hello function
 def sayHello(drone):
     print("Hello " + drone)
     
+# Call the function
 sayHello("Tello")
-
