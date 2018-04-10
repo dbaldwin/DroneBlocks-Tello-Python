cd23233f5556f7e27455c3ba1e82d1999808e099
Dennis Baldwin <dennisbaldwin@gmail.com>
2018-04-09 19:47:04 -0500

    Preliminary box mission code

---
+++ lesson4-box-mission/TelloBoxMission.py
@@ -0,0 +1,89 @@
+# This example script demonstrates how use Python to fly Tello in a box mission
+# This script is part of our course on Tello drone programming
+# https://learn.droneblocks.io/p/tello-drone-programming-with-python/
+
+# Import the necessary modules
+import socket
+import threading
+import time
+
+# IP and port of Tello
+tello_address = ('192.168.10.1', 8889)
+
+# IP and port of local computer
+local_address = ('', 9000)
+
+# Create a UDP connection that we'll send the command to
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind to the local address and port
+sock.bind(local_address)
+
+# Send the message to Tello and allow for a delay in seconds
+def send(message, delay):
+  # Try to send the message otherwise print the exception
+  try:
+    sock.sendto(message.encode(), tello_address)
+    print("Sending message: " + message)
+  except Exception as e:
+    print("Error sending: " + str(e))
+  
+  # Delay for a user-defined period of time
+  time.sleep(delay)
+
+# Receive the message from Tello
+def receive():
+  # Try to receive the message otherwise print the exception
+  try:
+    response, ip_address = sock.recvfrom(128)
+    print("Received message: " + response.decode(encoding='utf-8'))
+  except Exception as e:
+    print("Error receiving: " + str(e))
+
+# Create and start a listening thread that runs in the background
+# This utilizes our receive functions and will continuously monitor for incoming messages
+receiveThread = threading.Thread(target=receive)
+#receiveThread.daemon = True
+receiveThread.start()
+    
+# Each leg of the box will be 100 cm. Tello uses cm units by default.
+box_leg_distance = 100
+
+# Yaw 90 degrees
+yaw_angle = 90
+
+# Yaw clockwise (right)
+yaw_direction = "cw"
+
+# Put Tello into command mode
+send("command", 3)
+
+# Send the takeoff command
+send("takeoff", 5)
+
+# Fly forward
+send("forward " + str(box_leg_distance), 4)
+
+# Yaw right
+send("cw " + str(yaw_angle), 3)
+
+# Fly forward
+send("forward " + str(box_leg_distance), 4)
+
+# Yaw right
+send("cw " + str(yaw_angle), 3)
+
+# Fly forward
+send("forward " + str(box_leg_distance), 4)
+
+# Yaw right
+send("cw " + str(yaw_angle), 3)
+
+# Fly forward
+send("forward " + str(box_leg_distance), 4)
+
+# Yaw right
+send("cw " + str(yaw_angle), 3)
+
+# Land
+send("land", 5)
