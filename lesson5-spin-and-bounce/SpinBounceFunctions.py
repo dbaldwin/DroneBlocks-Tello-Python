43181972dd5c476d3dcb223459e0d77322851550
Dennis Baldwin <dennisbaldwin@gmail.com>
2018-04-18 16:08:24 -0500

    Spin tweaks

--- lesson5-spin-and-bounce/SpinBounceFunctions.py
+++ lesson5-spin-and-bounce/SpinBounceFunctions.py
@@ -51,20 +51,27 @@ receiveThread = threading.Thread(target=receive)
 receiveThread.daemon = True
 receiveThread.start()
 
-# Tello commands respond with an OK when sucessful. This means Tello asdf
-# recognizes the command, but the instruction hasn't completed. OK is Tello saying "I got the message"
-# but not necessarily saying "I completed the command"
-# This means we need to calculate how long the spin will take before we execute the next command.
-# Based on our tests a single 360 rotation takes 6 seconds. We'll use this in our spin function to delay
-# before the next command. Your rotation time my vary. This can be measured with a stop watch and implemented below.
-rotationTime = 6
-
+# Initiate command mode and takeoff
 def takeoff():
+  send("command", 3)
   send("takeoff", 5)
 
+# Land
 def land():
   send("land", 5)
 
+# Tello commands respond with an OK when sucessful. This means Tello recognizes
+# the command, but the instruction hasn't completed. OK is Tello saying "I got
+# the message" but not necessarily saying "I completed the command"
+# This means we need to calculate how long the spin will take before we execute the next command.
+# Based on our tests a single 360 rotation takes 7 seconds. We'll use this in our spin function to delay
+# before the next command. Your rotation time may vary. You can calculate this by
+# sending a "cw 360" or "ccw 360" command and measuring the rotation time.
+
+# 7 seconds per rotation
+rotationTime = 7
+
+# Spin right or left X number of times
 def spin(direction, times):
   # One rotation is 360 degrees
   oneRotation = 360
