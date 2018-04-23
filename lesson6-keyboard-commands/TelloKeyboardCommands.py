1890e557a3c18988d8258da911b3594f8d73c5ff
Dennis Baldwin <dennisbaldwin@gmail.com>
2018-04-23 09:51:46 -0500

    Comments

--- lesson6-keyboard-commands/TelloKeyboardCommands.py
+++ lesson6-keyboard-commands/TelloKeyboardCommands.py
@@ -44,20 +44,26 @@ receiveThread = threading.Thread(target=receive)
 receiveThread.daemon = True
 receiveThread.start()
 
+# Tell the user what to do
 print('Type in a Tello SDK command and press the enter key. Enter "quit" to exit this program.')
 
+# Loop infinitely waiting for commands or until the user types quit or ctrl-c
 while True:
   
   try:
+    # Read keybord input from the user
     message = input('')
     
+    # If user types quit then let's exit and close the socket
     if 'quit' in message:
       print("Program exited sucessfully")
       sock.close()
       break
-      
+    
+    # Send the command to Tello
     send(message)
-  # Handle ctrl-c case
+    
+  # Handle ctrl-c case to exit and close the socket
   except KeyboardInterrupt as e:
     sock.close()
     break
